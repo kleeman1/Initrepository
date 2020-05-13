45ede7a2c1591fa270d47c30d6ad9778f4c4498f
Ken Leeman <leeman_kenneth@hotmail.com>
2020-05-12 21:57:13 -0500

    Initial Commit

---
+++ footer.php
@@ -0,0 +1,7 @@
+<?php
+
+	// include
+	echo "Copyright (c)" . $company_name .  " " .
+		date("Y") . " - All Rights Reserved";
+
+?>
---
+++ indexXX.php
@@ -0,0 +1,60 @@
+
+<h1>
+  <br>
+  <center>
+
+    	
+
+  	<?php
+
+  		//echo "Hello world";
+
+  		// For Loop
+  	/*	for($count = 0; $count <= 10; $count++)
+  		{
+  			echo "The count is $count <br/>";
+  		}  */
+
+  		// Foreach
+  	/* $names = array("John", "Steve", "Mary");
+
+  		foreach ($names as$ value) {
+  			echo "$value<br/>"; */
+
+        // functions
+      /*  function helloThere($num_1, $num_2) {
+          return $num_1 + $num_2;
+        }
+
+        $answer = helloThere(49, 1);
+        echo $answer * 1.1;  */
+
+        //Random function
+       // echo rand(0, 10);
+       // echo mt_rand(0, 10);
+
+      /*  echo "Guess a number!<br/>";
+        $names = array("John", "Steve", "Mary");
+        $rando = mt_rand(0, 2);
+        echo $names[$rando];  */
+
+  		/* echo "Guess a number!<br/>";
+        $num = 4;
+        $rando = mt_rand(0, 10);
+        if ($num == $rando){
+          echo "YOU WIN $num = $rando";
+          }  else {
+            echo "YOU Lose! $num doesn't equal $rando";
+          }  */
+
+        // String manipulation see manual
+
+          
+
+
+        
+
+ 	 ?>
+  </center>
+  <br/>
+</h1>
